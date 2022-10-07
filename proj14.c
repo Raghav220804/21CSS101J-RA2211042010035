07efffe5d5506f2bc94dcd2c1c86709373a861f1
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 22:44:43 +0530

    Add files via upload

---
+++ proj14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float number1,number2,result;
+    printf("Enter one integer:");
+    scanf("%f",&number1);
+    printf("enter second integer:");
+    scanf("%f",&number2);
+    result=number1/number2;
+    printf("%f",result);
+    return 0;
+}
