bf59ec820a463211263ae8e7990bc727372a987e
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 23:32:11 +0530

    Add files via upload

---
+++ proj19.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int base,height,area;
+    printf("enter the base:");
+    scanf("%d",&base);
+    printf("enter the height:");
+    scanf("%d",&height);
+    area=0.5*base*height;
+    printf("%d",area);
+    return 0;
+}
