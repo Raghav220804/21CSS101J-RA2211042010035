4cad6ec318ec27bbf7023f76c981b7d4a75f29f7
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 23:39:08 +0530

    Add files via upload

---
+++ proj22.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    float radius,height,csa;
+    printf("enter the radius:");
+    scanf("%f",&radius);
+    printf("enter the height:");
+    scanf("%f",&height);
+    printf("%f",csa=2*radius*height*3.14);
+    return 0;
+}
