5df7af8a4526a3ae5160baacc05acaef57d77a29
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 17:28:15 +0530

    Add files via upload

---
+++ proj8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+float a,b,c,product; 
+printf("enter first number:",a);
+scanf("%f",&a);
+printf("enter second number:",b);
+scanf("%f",&b);
+printf("enter third number:",c);
+scanf("%f",&c);
+product=a*b*c;
+printf("%f",product);
+return 0;
+}
