9dfc2d5d4c49658996546f9265b1adb09a4edc80
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 12:50:05 +0530

    Add files via upload

---
+++ proj5.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+int main()
+{
+    int number1,number2,result;
+    printf("enter the number1:\n");
+    scanf("%d",&number1);
+    printf("enter the number2:");
+    scanf("%d",&number2);
+    result=number2-number1;
+    printf("%d",result);
+    return 0;
+}
