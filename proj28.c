fb46f3162b4618b527bf688f8dbc2f846bc5d109
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-08 00:53:20 +0530

    Add files via upload

---
+++ proj28.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float farenheit,celcius;
+    printf("enter the temprature in farenhiet:");
+    scanf("%f",&farenheit);
+    celcius=5.0/9.0*(farenheit-32.0);
+    printf("temprature is:%f",celcius);
+    return 0;
+}
