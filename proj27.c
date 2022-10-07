46036c0cfa41da0ea44574f28ea1499cdca49a91
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-08 00:47:40 +0530

    Add files via upload

---
+++ proj27.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float radius,height,volume;
+    printf("enter the radius");
+    scanf("%f",&radius);
+    printf("enter the height");
+    scanf("%f",&height);
+    printf("%f",volume=3.14*radius*radius*height);
+    return 0;
+}
