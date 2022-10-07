94a57c11a635289a70f77b7506915d19c39ab9e5
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 23:24:02 +0530

    Add files via upload

---
+++ proj17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   float a,b;
+   printf("enter one number: ");
+   scanf("%f",&a);
+   printf("enter another number to compare: ");
+   scanf("%f",&b);
+   if(a>b)
+   printf("the biggest among the numbers is %f",a);
+   else
+   printf("the biggest among the number is %f",b);
+  
+    return 0;
+}
