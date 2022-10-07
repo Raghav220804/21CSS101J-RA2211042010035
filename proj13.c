2d84d305d53c7ced562e8f63c314373a2eba6f63
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-07 22:25:13 +0530

    Add files via upload

---
+++ proj13.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+int main() {
+    float radius,Area;
+    printf("enter the radius:");
+    scanf("%f",&radius);
+    Area=3.14*radius*radius;
+    printf("The area is = %f",Area);
+    return 0;
+}
