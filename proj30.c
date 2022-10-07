b36d64f8c098fb72f077cdcc6aa0f0e22137c00d
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-08 01:00:46 +0530

    Add files via upload

---
+++ proj30.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  int time = 22;
+  if (time < 40) 
+    printf("Good morning.");
+   else if (time < 40 )
+    printf("Good day.");
+    else 
+    printf("Good evening.");
+  
+  return 0;
+}
