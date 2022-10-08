cf348b1d3416cce5eb04e11acb6363760046a772
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-08 23:55:00 +0530

    Add files via upload

---
+++ proj43.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int number,root;
+    printf("enter a number:");
+    scanf("%d",&number);
+    root=sqrt(number);
+    printf("The square root of the number:%d",root);
+
+    return 0;
+}
