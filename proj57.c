b786c1ce45592dc9d67d88064e648707a6177d02
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-09 12:57:10 +0530

    Add files via upload

---
+++ proj57.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  // Name of Institute
+  printf("SRM institute of Science and Technology\n");
+  // Name of Student 
+  printf("Raghavendra \n");
+  // Course
+  printf("B.Tech in Computer Science and Buissness Systems\n");
+  // Regsitration Number
+  printf("RA2211042010035\n");
+  
+  return 0;
+}
