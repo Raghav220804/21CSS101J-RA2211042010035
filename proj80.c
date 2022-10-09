736aa8083b967fad6db7bd65d005140d3a111856
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-09 22:36:11 +0530

    Add files via upload

---
+++ proj80.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float a, b, h;
+    float area;
+ 
+    printf("Enter the value for two bases & height of the trapezium: \n");
+    scanf("%f%f%f", &a, &b, &h);
+    area = 0.5 * (a + b) * h ;
+    printf("Area of the trapezium is: %.3f", area);
+    return 0;
+}
