c4335f4a8f7502fd1a57201153b4aca1eceae1bb
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-09 13:06:33 +0530

    Add files via upload

---
+++ proj59.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    char c;
+    printf("enter the alphabet");
+    scanf("%c",&c);
+    switch(c)
+    {
+        case 'a':
+        printf("\n\n entered alphabet is a vowel");
+        scanf("%c",&c);
+        break;
+        case 'e':
+        printf("\n\n entered alphabet is a vowel");
+        scanf("%c",&c);
+        break;
+        case 'i':
+        printf("\n\n entered alphabet is a vowel");
+        scanf("%c",&c);
+        break;
+        case 'o':
+        printf("\n\n entered alphabet is a vowel");
+        scanf("%c",&c);
+        break;
+        case 'u':
+        printf("\n\n entered alphabet is a vowel");
+        scanf("%c",&c);
+        break;
+        default:printf("the enetered alphabet is not a vowel");
+    }
+}
