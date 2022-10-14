562149225e7eb97e6153f1f8e836fbb87a208c33
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-14 15:58:27 +0530

    Update 6. area of rectangle.c

--- 6. area of rectangle.c
+++ 6. area of rectangle.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 float main() {
+    printf("RA2211042010035 \n");
     float length,breadth,area;
     printf("enter the length and breadth:");
     scanf("%f %f",&length,&breadth);
