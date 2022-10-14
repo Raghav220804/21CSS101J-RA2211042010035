b68f82fc101e493f1774e211423fdc132fcdf391
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-14 15:35:30 +0530

    Update 28. Fahrenheit to Celsius.c

--- 28. Fahrenheit to Celsius.c
+++ 28. Fahrenheit to Celsius.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    printf("RA2211042010035 \n")
+    printf("RA2211042010035 \n");
     float farenheit,celcius;
     printf("enter the temprature in farenhiet:");
     scanf("%f",&farenheit);
