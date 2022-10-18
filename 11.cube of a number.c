4566feb99e10b12bd9a9087af1c9b8375cdac33d
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-10-18 07:35:13 +0530

    Update 11.cube of a number.c

--- 11.cube of a number.c
+++ 11.cube of a number.c
@@ -2,8 +2,7 @@
 int main() {
   printf("RA2211042010035 \n");
 int i,cube; 
-//enter any desired value for the numbe//
-printf("enter a number");
+printf("enter a number"); 
 scanf("%d",&i);
 cube=i*i*i;
 printf("%d",cube);
