68d456937b6bd8a86037448ae68896d56f1caea3
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-20 03:22:24 +0530

    Create 82. print largest number.c

---
+++ 82. print largest number.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(){
+  printf("RA22110420010035\n");
+  int a,b;
+  printf("Enter two numbers : ");
+  scanf("%d %d",&a,&b);
+  if(a>b)
+    printf("The largest number : %d\n",a);
+  if (b>a)
+    printf("The largest number : %d\n",b);
+  printf("The smallest number : %d\n",a);
+}
