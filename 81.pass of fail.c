c99ea76956befb445546d85a275f1b8064a6f0ce
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-20 03:10:32 +0530

    Create 81.pass of fail.c

---
+++ 81.pass of fail.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+printf("RA2211042010035\n");
+int marks;
+	printf("Enter marks : ");
+	scanf("%d",&marks);
+	if(marks >= 40)
+		printf("\nResult is pass");
+	else
+		printf("\nResult is fail");
+	return 0;
+}
