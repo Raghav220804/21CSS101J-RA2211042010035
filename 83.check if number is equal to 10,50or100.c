e55d2fd6964da124533ea9f24b77a78e7c5886b5
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-20 03:27:39 +0530

    Create 83.check if number is equal to 10,50or100.c

---
+++ 83.check if number is equal to 10,50or100.c
@@ -0,0 +1,20 @@
+#include<stdio.h>    
+int main(){    
+    printf("RA2211042010035\n");
+int a=0;    
+printf("Enter a number: ");    
+scanf("%d",&a);     
+if(a==10){    
+printf("Number is equal to 10");    
+}    
+else if(a==50){    
+printf("Number is equal to 50");    
+}    
+else if(a==100){    
+printf("Number is equal to 100");    
+}    
+else{    
+printf("Number is not equal to 10, 50 or 100");    
+}    
+return 0;  
+}    
