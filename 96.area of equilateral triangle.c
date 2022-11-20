52cc75a7b0b070507a142b5f9c4fe207738f6d26
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-21 03:54:29 +0530

    Create 96.area of equilateral triangle.c

---
+++ 96.area of equilateral triangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010035\n");
+   int area, side;
+   printf("Side = ");
+   scanf("%d", &side);
+   area= 0.433*side*side;
+   printf("%d", area);
+ }
