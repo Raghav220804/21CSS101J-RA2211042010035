326f68fa646a3961db127fc80855c4b00535fb65
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-21 03:52:02 +0530

    Create 96.print largest and smallest.c

---
+++ 96.print largest and smallest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010035\n");
+   int a,b;
+ printf("Enter two numbers : ");
+   scanf("%d %d", &a, &b);
+   if(a>b) 
+     printf("The largest number : %d\nThe smallest number : %d\n", a, b);
+     else 
+       printf("The largest number : %d\nThe smallest number : %d\n", b, a);
+      }
