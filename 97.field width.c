9d2073897568c63c0bd39737393a9a907101f891
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-21 03:57:51 +0530

    Create 97.field width.c

---
+++ 97.field width.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010035\n");
+   int a;
+   printf("Number : ");
+   scanf("%d",&a);
+   printf("%2d field width\n",a);
+   printf("%6d field width\n",a);
+   printf("%-6d with minus sign\n",a);
+   return 0;
+ }
+   
