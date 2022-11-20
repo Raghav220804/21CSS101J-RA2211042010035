111dde387bf5cbc83902d90dabd79681961bf1bd
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-21 03:45:09 +0530

    Update 90. SWAP using #define.c

--- 90. SWAP using #define.c
+++ 90. SWAP using #define.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #define SWAP(a,b,temp)(temp = a , a = b, b = temp)
 int main(){
+    printf("RA2211042010035\n");
     int x = 100 , y = 20 , temp = 0;
     SWAP(x,y,temp);
     printf("x = %d, y = %d\n",x,y);
