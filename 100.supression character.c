f4e00de8ed5ebf47f8fac23b51b8f59314f079ed
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-21 04:32:18 +0530

    Create 100.supression character.c

---
+++ 100.supression character.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("RA2211042010035 \n");
+    int a,b;
+    printf("Enter a 7 digit number : ");
+    scanf("%3d%*2d%2d",&a,&b);
+    printf("The values in a and b are : %d,%d\n",a,b);
+    return 0;
+}
