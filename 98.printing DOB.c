98e4051c0bc479d96b61afd9c33457745d396fd9
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-21 04:13:41 +0530

    Create 98.printing DOB.c

---
+++ 98.printing DOB.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010035\n");
+   int year,month,date;
+   printf("Enter DOB in yyyy.mm.dd : ");
+   scanf("%d.%d.%d",&year,&month,&date);
+   printf("DOB is : %d.%d.%d",year,month,date);
+   return 0;
+ }
