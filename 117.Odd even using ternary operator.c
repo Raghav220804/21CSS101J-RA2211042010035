53a9571d4eacf125ce5bee152a8dd2cd3f52585c
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 03:12:03 +0530

    Create 117.Odd even using ternary operator.c

---
+++ 117.Odd even using ternary operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+ {
+   printf("RA2211042010035\n");
+   int n;
+   printf("n = ");
+   scanf("%d", &n);
+   ( n % 2 == 0) ? printf("%d is even\n", n) : printf("%d is odd\n", n);
+   return 0;
+ }
