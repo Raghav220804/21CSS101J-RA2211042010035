a326449283fa014ae4dea2548a6a723564bed983
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 02:32:03 +0530

    Create 107.Finding dimensions of a rectangular box.c

---
+++ 107.Finding dimensions of a rectangular box.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    printf("RA2211042010035\n");
+    int number;
+    int N,M;
+    printf("Enter the dimensions of box\n");
+    scanf("%d%d",&N,&M);
+    number=N*M;
+    printf("Number of containers required=%d",number);
+}
