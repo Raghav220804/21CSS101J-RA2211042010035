7f238e82d8e6569937613a8759a8525fffe315b1
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 03:02:05 +0530

    Create 113.Check if number is divisible by 5.c

---
+++ 113.Check if number is divisible by 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010035\n");
+   int number;
+   printf("enter the number : ");
+   scanf("%d",&number);
+   if (number % 5 == 0 ) {
+   printf("The number is divisible by 5\n"); 
+   }
+   else {
+   printf(" The number is not divisible by 5 ");
+   }
+ }
