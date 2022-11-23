d4af7ecfb8b2ac15350c84986dff1f339720313e
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 02:38:47 +0530

    Create 108.print address of a variable.c

---
+++ 108.print address of a variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main () {
+   printf("RA2211042010035\n");
+   char a;
+   int x;
+   float p, q;
+   a = 'A';
+   x = 125;
+   p = 10.25, q = 18.76;
+   printf("%c is stored at addr %u.\n", a, &a);
+   printf("%d is stored at addr %u.\n", x, &x);
+   printf("%f is stored at addr &u.\n", p, &p);
+   printf("%f is stored at addr %u.\n", q, &q);
+ }
