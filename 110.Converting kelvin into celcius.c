f7c67356c0b3026e1955fe90fa22ca1eca90cfb0
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 02:45:55 +0530

    Create 110.Converting kelvin into celcius.c

---
+++ 110.Converting kelvin into celcius.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+ int main() {
+ printf("RA2211042010035\n");
+
+ float kelvin,celsius;
+ printf("Enter value of kelvin :");
+ scanf("%f", &kelvin);
+ celsius=kelvin+273.15;
+ printf("%0.2f Kelvin is %0.2f Celsius", kelvin , celsius);
+ }
