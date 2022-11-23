39acd1787c72ece7133269ea33a6d952b5b1c7f3
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 03:15:00 +0530

    Create 118.Looping using while.c

---
+++ 118.Looping using while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int main() {
+
+     printf("RA2211042010035\n");
+     int i=1;
+      while (i<10) {
+
+          printf("%d\n", i);
+          ++i;
+      }
+      return 0;
+
+ }     
