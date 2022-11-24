0b43d85513504f530207fd2f272d4b500df094f0
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-24 11:16:32 +0530

    Create 119.check if numbers are equal or not.c

---
+++ 119.check if numbers are equal or not.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+    printf("RA2211042010035\n");
+    int int1, int2;
+    printf("enter two numbers: ");
+    scanf("%d%d",&int1,&int2);
+    if (int1==int2)
+        printf("the numbers are equal\n");
+    else
+        printf("the numbers are not equal\n");
+}
