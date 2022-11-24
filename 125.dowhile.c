a7e54301872263b50f86772bf44e2c335317531f
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2022-11-25 00:30:43 +0530

    Create 125.dowhile.c

---
+++ 125.dowhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("RA2211042010035\n");
+    printf("Enter a number");
+    int n;
+    scanf("%d",&n);
+    int i=0;
+    do{
+        printf("%d",i++);
+    }while(i>10);
+}
